53ff65e633a9ca5482442f1fa99a50af6fab26cb
KattachaithanyaKumar <80614118+KattachaithanyaKumar@users.noreply.github.com>
2021-09-17 00:24:43 +0530

    Update maincode.java

--- maincode.java
+++ maincode.java
@@ -1,19 +1,14 @@
-/* The code doesnot work yet */
-int size = 30;
-int[][] grid = new int[size][size];
-int x = 10;
-int y = 10;
-int xx = size / 2;
-int yy = size / 2;
-int cdir = floor(random(3));
-int odds = 1;
-int cx = xx;
-int cy = yy;
-int wi = size / 2;
-int hi = size / 2;
-int count = 0;
-int xdir = 0;
-int ydir = 0;
+int size = 10;
+int wh = 90;
+int[][] grid = new int[wh][wh];
+int xx = 10;
+int yy = 10;
+int w = wh;
+int h = wh;
+int px = xx;//floor(random(size));
+int py = yy;//floor(random(size));
+int ox = px;
+int oy = py;
 /*
 0 - water
 1 - grass
@@ -24,36 +19,34 @@ void setup()  {
   size(920, 920);
   //fullScreen();
   background(0);
-  for (int i = 0; i < size; i++)  {
-    for (int j = 0; j < size; j++)  {
+  for (int i = 0; i < wh; i++)  {
+    for (int j = 0; j < wh; j++)  {
       //grid[i][j] = floor(random(3));
       grid[i][j] = 0;
     }
   }
-  grid[xx][yy] = 1;
+  //grid[xx][yy] = 1;
 }
 
-int r = 0;
-
 void draw()  {
-  
-  generate();
-  
-  for (int i = 0; i < size; i++)  {
-    for (int j = 0; j < size; j++)  {
+
+  dogen();
+  design();
+  for (int i = 0; i < wh; i++)  {
+    for (int j = 0; j < wh; j++)  {
       int s = size;
         
       if (grid[i][j] == 0)  {
         //water
-        rect(x + (i * s), y + (j * s), s, s);
+        rect(xx + (i * s), yy + (j * s), s, s);
         fill(0,0,255);
       }else if (grid[i][j] == 1)  {
         //grass
-        rect(x + (i * s), y + (j * s), s, s);
+        rect(xx + (i * s), yy + (j * s), s, s);
         fill(0,255,0);
       }else if (grid[i][j] == 2)  {
         //sand
-        rect(x + (i * s), y + (j * s), s, s);
+        rect(xx + (i * s), yy + (j * s), s, s);
         fill(255,255,0);
       }
       
@@ -61,42 +54,52 @@ void draw()  {
   }
 }
 
+void ex(int sx, int sy)  {
+  int radius = floor(random(10));
+  int top =    sy - radius,
+    bottom = sy + radius;
 
-void generate()  {
-  for (int i = 0; i < 1000; i++)  {
-  grid[cx][cy] = 1;
-  int o = floor(random(odds));  
-  if (o == odds)  {
-  cdir = floor(random(3));
-  }
-  if (cdir == 0)  {
-    xdir = 1;
-    ydir = 0;
-  }else if (cdir == 1)  {
-    xdir = 0;
-    ydir = -1;
-  }else if (cdir == 2)  {
-    xdir = -1;
-    ydir = 0;
-  }else if (cdir == 3)  {
-    xdir = 0;
-    ydir = 1;
-  }
-    
-  cx += xdir;
-  cy += ydir;
-  cx = clamp(cx, 1, wi-1);
-  cy = clamp(cy, 1, hi-1);
+    for (int y = top; y <= bottom; y++) {
+      int   dy  = y - sy;
+      float dx  = sqrt(radius*radius - dy*dy);
+      int left  = ceil(sx - dx),
+          right = floor(sx + dx);
+      for (int x = left; x <= right; x++) {
+        // draw tile (x, y)   
+       grid[x][y] = 1;
+       System.out.println("x: " + x + " y: " + y);
+     }
   }
   noLoop();
 }
 
-int clamp(int a, int low, int high)  {
-  if (a <= low)  {
-    a = low;
+void dogen()  {
+  for (int i = 0; i < 50; i++)  {
+    int _x = floor(random(wh));
+    int _y = floor(random(wh));
+    if ((_x < 80)&&(_x > 10)&&(_y > 10)&&(_y < 80))  {
+      grid[_x][_y] = 1;
+      ex(_x, _y);
+    }
   }
-  if (a >= high)  {
-    a = high;
+  noLoop();
+}
+
+void design()  {
+  for (int i = 5; i < 85; i++)  {
+    for (int j = 5; j < 85; j++)  {
+      if ((grid[i][j] == 1)&&(grid[i+1][j] == 0))  {
+        grid[i][j] = 2;
+      }
+      if ((grid[i][j] == 1)&&(grid[i-1][j] == 0))  {
+        grid[i][j] = 2;
+      }
+      if ((grid[i][j] == 1)&&(grid[i][j+1] == 0))  {
+        grid[i][j] = 2;
+      }
+      if ((grid[i][j] == 1)&&(grid[i][j-1] == 0))  {
+        grid[i][j] = 2;
+      }
+    }
   }
-  return a;
 }
